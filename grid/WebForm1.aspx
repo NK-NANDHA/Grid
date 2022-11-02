437c5ff14a18a99a8af6fbbb2e856bf67000aad1
NK-NANDHA <nk7473755@gmail.com>
2022-11-02 15:06:45 +0530

    Add project files.

---
+++ grid/WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="grid.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False"
+OnRowDataBound="OnRowDataBound" OnRowEditing="OnRowEditing" OnRowCancelingEdit="OnRowCancelingEdit"
+OnRowUpdating="OnRowUpdating" OnRowDeleting="OnRowDeleting" EmptyDataText="No records has been added." DataSourceID="CustomerId">
+</asp:GridView>
+<table border="1" cellpadding="0" cellspacing="0" style="border-collapse: collapse">
+<tr>
+    <td style="width: 150px">
+        Name:<br />
+        <asp:TextBox ID="txtName" runat="server" Width="140" />
+    </td>
+    <td style="width: 150px">
+        Country:<br />
+        <asp:TextBox ID="txtCountry" runat="server" Width="140" />
+    </td>
+    <td style="width: 100px">
+        <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="Insert" />
+    </td>
+</tr>
+</table>
+        
+    </form>
+</body>
+</html>
---
+++ grid/WebForm1.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace grid
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
+
+        }
+        private void BindGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["ABCD"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("Customers_CRUD"))
+                {
+                    cmd.Parameters.AddWithValue("@Action", "SELECT");
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            GridView1.DataSource = dt;
+                            GridView1.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+
+
+        
+            protected void Insert(object sender, EventArgs e)
+        {
+                string name = txtName.Text;
+                string country = txtCountry.Text;
+                string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    using (SqlCommand cmd = new SqlCommand("Customers_CRUD"))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@Action", "INSERT");
+                        cmd.Parameters.AddWithValue("@Name", name);
+                        cmd.Parameters.AddWithValue("@Country", country);
+                        cmd.Connection = con;
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+                this.BindGrid();
+            }
+        }
+    }
